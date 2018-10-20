8ff349098be9efb7f58216f1e6bb60e7f680da37
daart <artemko51@gmail.com>
2018-10-20 22:20:47 +0300

    tested on 100*100 cells grid

--- src/App.js
+++ src/App.js
@@ -9,13 +9,19 @@ class App extends Component {
 
     this.size = this.props.size || 10;
     this.state = {
-      grid: [] 
-    }
+      grid: []
+    };
   }
-  
+
+  componentDidMount() {
+    this.reset();
+  }
+
   createEmptyMatrix = () => {
-    let matrix = new Array(this.size).fill(0).map(row => new Array(this.size).fill(0))
-    
+    let matrix = new Array(this.size)
+      .fill(0)
+      .map(row => new Array(this.size).fill(0));
+
     return matrix;
   }
 
@@ -27,93 +33,77 @@ class App extends Component {
   reset = () => {
     this.pause();
 
-    let startingGrid = this.createEmptyMatrix()
-      .map(row => row
-        .map(cell => Math.round(Math.random())));
+    let startingGrid = this.createEmptyMatrix().map(row =>
+      row.map(cell => Math.round(Math.random()))
+    );
 
     this.setState({
       grid: startingGrid
-      
     });
-    
-  }
+  };
 
-  componentDidMount() {
-    this.reset();
-  }
-  
   next = () => {
     const { size } = this;
     const { grid } = this.state;
     let iterationsCnt = 0;
     let newGrid = this.createEmptyMatrix();
     let visitedDead = new Set();
-    
+
     for (let y = 0; y < size; y += 1) {
       for (let x = 0; x < size; x += 1) {
         iterationsCnt += 1;
-        
+
         if (grid[y][x] === 1) {
           let alive = this.countAlive(y, x);
           iterationsCnt += 9;
-          
+
           if (alive === 2 || alive === 3) {
             newGrid[y][x] = 1;
           }
-          
-          for (let i = - 1; i < 2; i += 1) {
-            for (let j = - 1; j < 2; j += 1) {
+
+          for (let i = -1; i < 2; i += 1) {
+            for (let j = -1; j < 2; j += 1) {
               iterationsCnt += 1;
-              
+
               if (i === 0 && j === 0) continue;
-              
-              let yy = (y + i);
-              let xx = (x + j);
-              
-              if (yy < 0 || yy > size - 1 || xx < 0 || xx > size - 1) {
-                continue;
-              }
+
+              let yy = (y + i + size) % size;
+              let xx = (x + j + size) % size;
 
               if (grid[yy][xx] === 0) {
-                if (visitedDead.has(`${yy}-${xx}`)) continue
-                
+                if (visitedDead.has(`${yy}-${xx}`)) continue;
+
                 visitedDead.add(`${yy}-${xx}`);
-                
+
                 let alive = this.countAlive(yy, xx);
-                
+
                 if (alive === 3) {
                   newGrid[yy][xx] = 1;
                 }
-                
               }
             }
           }
-          
         }
       }
     }
-      
+
     this.setState({
       grid: newGrid
-    })
+    });
 
     // for perfomance testing purpose
-    console.log('iterations: ', iterationsCnt); 
+    console.log("iterations: ", iterationsCnt);
   }
 
   countAlive = (y, x) => {
     const { size } = this;
     let { grid } = this.state;
     let alive = 0;
-    
+
     for (let i = -1; i < 2; i += 1) {
       for (let j = -1; j < 2; j += 1) {
-        let yy = (y + i + this.size) % this.size;
-        let xx = (x + j + this.size) % this.size;
-
-        if (yy < 0 || yy > size - 1 || xx < 0 || xx > size - 1) {
-          continue;
-        }
+        let yy = (y + i + size) % size;
+        let xx = (x + j + size) % size;
 
         alive += grid[yy][xx];
       }
@@ -121,7 +111,7 @@ class App extends Component {
 
     alive -= grid[y][x];
 
-    return alive; 
+    return alive;
   }
 
   pause = () => {
@@ -135,23 +125,28 @@ class App extends Component {
       <Grid centered>
         <div className="App">
           <div id="grid">
-            {
-              grid.map((row, y) => (
-                row.map((cell, x) => cell === 1 ? (
-                  <div
-                    className="cell"
-                    key={`${y}-${x}`}
-                    style={{
-                      left: x * 5,
-                      top: y * 5
-                    }}              
-                  >
-                  </div>
-                ) : null)
-              ))
-            }
+            {grid.map((row, y) =>
+              row.map(
+                (cell, x) =>
+                  cell === 1 ? (
+                    <div
+                      className="cell"
+                      key={`${y}-${x}`}
+                      style={{
+                        left: x * 5,
+                        top: y * 5
+                      }}
+                    />
+                  ) : null
+              )
+            )}
           </div>
-          <Controls reset={this.reset} tick={this.tick} pause={this.pause} next={this.next} />
+          <Controls
+            reset={this.reset}
+            tick={this.tick}
+            pause={this.pause}
+            next={this.next}
+          />
         </div>
       </Grid>
     );
